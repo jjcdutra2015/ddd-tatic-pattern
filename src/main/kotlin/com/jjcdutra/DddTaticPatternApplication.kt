6057a2fb27c0fcc0f05a9002623f4ffdf0435122
jjcdutra2015 <jjcdutra@gmail.com>
2023-01-02 19:23:41 -0300

    first commit

---
+++ src/main/kotlin/com/jjcdutra/DddTaticPatternApplication.kt
@@ -0,0 +1,11 @@
+package com.jjcdutra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddTaticPatternApplication
+
+fun main(args: Array<String>) {
+    runApplication<DddTaticPatternApplication>(*args)
+}
---
+++ src/test/kotlin/com/jjcdutra/DddTaticPatternApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jjcdutra
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddTaticPatternApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
