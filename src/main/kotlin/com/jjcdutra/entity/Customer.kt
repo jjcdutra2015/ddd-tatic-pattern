aee4d406f50703a7d43b6737c55301481ba199ec
jjcdutra2015 <jjcdutra@gmail.com>
2023-01-02 20:04:30 -0300

    Principio da autoavaliação

--- src/main/kotlin/com/jjcdutra/entity/Customer.kt
+++ src/main/kotlin/com/jjcdutra/entity/Customer.kt
@@ -1,21 +1,37 @@
 package com.jjcdutra.entity
 
 class Customer(
-    val id: String,
+    private val id: String,
     private var name: String,
     private var address: String,
-    private var active: Boolean = true
+    private var active: Boolean = false
 ) {
+    init {
+        validate()
+    }
 
     fun changeName(name: String) {
         this.name = name
+        validate()
     }
 
     fun activate() {
+        if (this.address.isEmpty()) {
+            throw Exception("Address is mandatory to activate a customer")
+        }
         this.active = true
     }
 
     fun deactivate() {
         this.active = false
+    }
+
+    private fun validate() {
+        if (this.name.isEmpty()) {
+            throw Exception("Name is required")
+        }
+        if (this.id.isEmpty()) {
+            throw Exception("Id is required")
+        }
     }
 }
